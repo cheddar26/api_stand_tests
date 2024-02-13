6fcc63de4430977a6fb613dbc66e23b5d2cb3ff5
Cesar <marcelino.cesar26@gmail.com>
2024-02-13 13:13:19 -0600

    nuevo archivo

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-ebce39fa-8b01-40e8-9a72-3c2bbe7d2456.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
